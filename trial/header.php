9730da2e10305c256598c1021ac547dee0e3e3b7
Hussein Majed <hkmajed@hotmail.com>
2024-03-22 13:46:35 -0400

    finalized the index page,changed and created css style files to be more convenient

--- trial/header.php
+++ trial/header.php
@@ -1,21 +1,21 @@
+<header>
+    <nav class="navbar">
 
-<nav class="navbar">
+        <div class="logo">
+            <image src="images/blackcat.png"></image>
+            <a href="#">KronoKat</a>
+        </div>
 
-    <div class="logo">
-        <image src="images/blackcat.png"></image>
-        <a href="#">KronoKat</a>
-    </div>
+        <ul class="navdiv-ul">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="./signup.php" class="active">Signup</a></li>
+            <li><a href="./contact.html">Login</a></li>
+        </ul>
 
-    <ul class="navdiv-ul">
-        <li><a href="index.php" >Home</a></li>
-        <li><a href="./signup.php" class="active">Signup</a></li>
-        <li><a href="./contact.html">Login</a></li>
-    </ul>
-
-    <div class="hamburger">
-        <span class="bar"></span>
-        <span class="bar"></span>
-        <span class="bar"></span>
-    </div>
-
-</nav>
+        <div class="hamburger">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+    </nav>
+</header>
--- trial/index.php
+++ trial/index.php
@@ -1,91 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styles/style.css">
-    <link rel="stylesheet" type="text/css" href="styles/indexcss.css">
+    <link rel="stylesheet" href="styles/style.css">
     <title>KronoKat - Manage your tasks</title>
 </head>
+
 <body>
-    
+
     <?php include('header.php'); ?>
 
-    <div class="page-containers" id="index_top_container" style="background-color: blue; margin-top:40px; padding: 2rem; display: flex;">
-        <div id="features_container" style="background-color: yellow;">
-            <div class="features">MANAGE YOUR TASKS EASILY</div>
-            <div class="features">STAY ON TRACK WITH YOUR PROJECTS</div>
-            <div class="features">WORK WITHIN A TEAM</div>
+    <section class="Home">
+        <div class="features_container">
+            <div id="Features">
+                <div class="features">MANAGE YOUR TASKS EASILY</div>
+                <div class="features">STAY ON TRACK WITH YOUR PROJECTS</div>
+                <div class="features">WORK WITHIN A TEAM</div>
+            </div>
+
+            <div id="kronokat_container">
+                <h1>KronoKat</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Fusce congue maximus suscipit. Phasellus ut justo fringilla, iaculis tellus eu, pulvinar purus.
+                    Nam mollis accumsan velit, id venenatis dolor blandit ut. Quisque bibendum gravida enim, id semper arcu.
+                </p>
+            </div>
         </div>
-        <div id="title_container" style="background-color: yellow;">
-            <h1>KronoKat</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Fusce congue maximus suscipit. Phasellus ut justo fringilla, iaculis tellus eu, pulvinar purus. 
-                Nam mollis accumsan velit, id venenatis dolor blandit ut. Quisque bibendum gravida enim, id semper arcu. 
-            </p>
+    </section>
+
+    <section class="About">
+        <div class="page-containers">
+            <div class="card-container">
+                <h2>Meow 1</h2>
+                <div class="card-text-paragraph">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae accusamus ipsum reiciendis officiis, ea molestiae corrupti recusandae ullam officia repellat nostrum, non asperiores modi, at et commodi architecto illum. Unde.
+                </div>
+            </div>
+            <div class="card-container">
+                <h2>Meow 2</h2>
+                <div class="card-text-paragraph">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae accusamus ipsum reiciendis officiis, ea molestiae corrupti recusandae ullam officia repellat nostrum, non asperiores modi, at et commodi architecto illum. Unde.
+                </div>
+            </div>
+            <div class="card-container">
+                <h2>Meow 3</h2>
+                <div class="card-text-paragraph">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae accusamus ipsum reiciendis officiis, ea molestiae corrupti recusandae ullam officia repellat nostrum, non asperiores modi, at et commodi architecto illum. Unde.
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="page-containers" sid="index_central_container">
-        <div class="card-container">
-            <h2>Meow 1</h2>
-            <div class="card-label">
-                <strong>Learning</strong>
-            </div>
-            <p>Published 2024-01-29</p>
-            <div class="card-text-header">
-                <h2>HTML & CSS Fundations</h2>
-            </div>
-            <div class="card-text-paragraph">
-                These languages are the backbone of every website, defining structure, content and presentation
-            </div>
-            <div class="card-footer">
-                <img class="card-footer-image" src="../assets/images/image-avatar.webp">
-                <div class="card-footer-name"><strong>Andrea Visani</strong></div>
-            </div> 
-        </div>
-        <div class="card-container">
-            <h2>Meow 2</h2>
-            <div class="card-label">
-                <strong>Learning</strong>
-            </div>
-            <p>Published 2024-01-29</p>
-            <div class="card-text-header">
-                <h2>HTML & CSS Fundations</h2>
-            </div>
-            <div class="card-text-paragraph">
-                These languages are the backbone of every website, defining structure, content and presentation
-            </div>
-            <div class="card-footer">
-                <img class="card-footer-image" src="../assets/images/image-avatar.webp">
-                <div class="card-footer-name"><strong>Andrea Visani</strong></div>
-            </div> 
-        </div>
-        <div class="card-container">
-            <h2>Meow 3</h2>
-            <div class="card-label">
-                <strong>Learning</strong>
-            </div>
-            <p>Published 2024-01-29</p>
-            <div class="card-text-header">
-                <h2>HTML & CSS Fundations</h2>
-            </div>
-            <div class="card-text-paragraph">
-                These languages are the backbone of every website, defining structure, content and presentation
-            </div>
-            <div class="card-footer">
-                <img class="card-footer-image" src="../assets/images/image-avatar.webp">
-                <div class="card-footer-name"><strong>Andrea Visani</strong></div>
-            </div> 
-        </div>
-        
+    </section>
 
 
-
-    </div>
-
-
-    
 </body>
+
 </html>
-
-
