2bfda5db81ef3ec8efbd3958cb395ada5c35cec0
Anugrah Rai <raianugrah2004@gmail.com>
2024-03-26 13:28:09 -0400

    added trial 2 folder

---
+++ trial 2/login.php
@@ -0,0 +1,55 @@
+<?php
+$is_invalid = false;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    
+    $mysqli = require __DIR__ . "/database.php";
+    
+    $sql = "SELECT * FROM user WHERE username = ?";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param("s", $_POST["login"]);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $user = $result->fetch_assoc();
+
+    if($user) {
+        if (password_verify($_POST["password"],$user["password_hash"])){
+            session_start(); 
+            $_SESSION["user_id"] = $user['id'];
+            header('Location: ./index.php');
+            exit; 
+        }
+    }
+    $is_invalid=true;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+</head>
+<body>
+    
+    <h1>Login</h1>
+
+    <?php
+        if ($is_invalid): ?>
+        <em>Invalid Login</em>
+    <?php endif; ?>
+
+    
+    <form method="post">
+        <label for="User-name">User-Name</label>
+        <input type="text" name="login" id="login">
+        
+        <label for="password">Password</label>
+        <input type="password" name="password" id="password">
+        
+        <button>Log in</button>
+    </form>
+    
+</body>
+</html>
---
+++ trial 2/signup.php
@@ -0,0 +1,68 @@
+
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="styles/style.css">
+        <title>Signup</title>
+    </head>
+    <body>
+        
+        <?php include('header.php'); ?>
+    
+        <div class="formcontainer">
+            <h1>Sign-Up</h1>
+            <hr>
+            <form action="process-signup.php" method="post" onsubmit="return validate();">
+                
+                <div class="textfield">
+                    <label for="firstName">First Name</label>
+                    <input type="text" name="fName" id="fname" placeholder="First Name">
+                </div>
+    
+                <div class="textfield">
+                    <label for="lastName">Last Name</label>
+                    <input type="text" name="lName" id="lname" placeholder="Last Name">
+                </div>
+    
+                <div class="datefield">
+                    <label for="dob">Date of Birth</label>
+                    <input type="date" name="dob" id="dob" placeholder="Date of Birth">
+                </div>
+    
+                <div class="textfield">
+                    <label for="email">Email Address</label>
+                    <input type="text" name="email" id="email" placeholder="Email">
+                </div>
+                
+                <div class="textfield">
+                    <label for="login">User Name</label>
+                    <input type="text" name="login" id="login" placeholder="User name">
+                </div>
+    
+                <div class="textfield">
+                    <label for="pass">Password</label>
+                    <input type="password" name="pass" id="pass" placeholder="Password">
+                </div>
+            
+                <div class="textfield">
+                    <label for="pass2">Re-type Password</label>
+                    <input type="password" id="pass2" placeholder="Password">
+                </div>
+    
+                <div class="checkbox">
+                    <input type="checkbox" name="terms" id="terms">
+                    <label for="terms">I agree to the terms and conditions</label>
+                </div>
+    
+                <button type="submit">Sign-Up</button>
+                <button type="reset">Reset</button>
+    
+            </form>
+        </div>
+        <script src="./js/script.js"></script>
+    </body>
+</html>
+    
+    
