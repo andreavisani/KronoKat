7bb5391e5fad4346e54a1e5ff27137e88233b273
Anugrah Rai <raianugrah2004@gmail.com>
2024-03-27 16:56:34 -0400

    index page restored magic man

--- trial 2/index.php
+++ trial 2/index.php
@@ -1,60 +1,18 @@
-<?php
-session_start();
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styles/style.css">
-    <link rel="stylesheet" type="text/css" href="styles/indexcss.css">
-    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="./styles/index.css">
+    <link rel="stylesheet" href="./styles/header.css">
     <title>KronoKat - Manage your tasks</title>
 </head>
 
 <body>
-    
 
     <?php include('header.php'); ?>
 
-    <div class="page-containers" id="index_top_container" style="background-color: blue; margin-top:40px; padding: 2rem; display: flex;">
-        <div id="features_container" style="background-color: yellow;">
-            <div class="features">MANAGE YOUR TASKS EASILY</div>
-            <div class="features">STAY ON TRACK WITH YOUR PROJECTS</div>
-            <div class="features">WORK WITHIN A TEAM</div>
-        </div>
-        <div id="title_container" style="background-color: yellow;">
-            <h1>KronoKat</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Fusce congue maximus suscipit. Phasellus ut justo fringilla, iaculis tellus eu, pulvinar purus. 
-                Nam mollis accumsan velit, id venenatis dolor blandit ut. Quisque bibendum gravida enim, id semper arcu. 
-            </p>
-        </div>
-    </div>
-    <div class="page-containers" sid="index_central_container">
-        <div class="card-container">
-            <h2>Meow 1</h2>
-            <div class="card-label">
-                <strong>Learning</strong>
-            </div>
-            <p>Published 2024-01-29</p>
-            <div class="card-text-header">
-                <h2>HTML & CSS Fundations</h2>
-            </div>
-            <div class="card-text-paragraph">
-                These languages are the backbone of every website, defining structure, content and presentation
-            </div>
-            <div class="card-footer">
-                <img class="card-footer-image" src="../assets/images/image-avatar.webp">
-                <div class="card-footer-name"><strong>Andrea Visani</strong></div>
-            </div> 
-        </div>
-        <div class="card-container">
-            <h2>Meow 2</h2>
-            <div class="card-label">
-                <strong>Learning</strong>
     <section class="Home">
         <div class="features_container">
             <div id="Features">
@@ -62,12 +20,6 @@ session_start();
                 <div class="features">STAY ON TRACK WITH YOUR PROJECTS</div>
                 <div class="features">WORK WITHIN A TEAM</div>
             </div>
-            <p>Published 2024-01-29</p>
-            <div class="card-text-header">
-                <h2>HTML & CSS Fundations</h2>
-            </div>
-            <div class="card-text-paragraph">
-                These languages are the backbone of every website, defining structure, content and presentation
 
             <div id="kronokat_container">
                 <h1>KronoKat</h1>
@@ -76,22 +28,7 @@ session_start();
                     Nam mollis accumsan velit, id venenatis dolor blandit ut. Quisque bibendum gravida enim, id semper arcu.
                 </p>
             </div>
-            <div class="card-footer">
-                <img class="card-footer-image" src="../assets/images/image-avatar.webp">
-                <div class="card-footer-name"><strong>Andrea Visani</strong></div>
-            </div> 
         </div>
-        <div class="card-container">
-            <h2>Meow 3</h2>
-            <div class="card-label">
-                <strong>Learning</strong>
-            </div>
-            <p>Published 2024-01-29</p>
-            <div class="card-text-header">
-                <h2>HTML & CSS Fundations</h2>
-            </div>
-            <div class="card-text-paragraph">
-                These languages are the backbone of every website, defining structure, content and presentation
     </section>
 
     <section class="About">
@@ -114,22 +51,10 @@ session_start();
                     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae accusamus ipsum reiciendis officiis, ea molestiae corrupti recusandae ullam officia repellat nostrum, non asperiores modi, at et commodi architecto illum. Unde.
                 </div>
             </div>
-            <div class="card-footer">
-                <img class="card-footer-image" src="../assets/images/image-avatar.webp">
-                <div class="card-footer-name"><strong>Andrea Visani</strong></div>
-            </div> 
         </div>
-        
     </section>
 
 
-
-    </div>
-
-
-
 </body>
-</html>
-
 
 </html>
