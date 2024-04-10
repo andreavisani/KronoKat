562774ff4a489efeaa98c3eeafeb25949fbceca1
Andrea Visani <visaniandrea.92@gmail.com>
2024-04-10 12:06:06 -0400

    reupload

--- scripts/footer.php
+++
@@ -1,9 +0,0 @@
-
-<footer class="footer">
-    <div class="footer-text">
-        <h1> KRRONOKAT </h1>
-    </div>
-    <div  class="final-copyright">
-    <p>Copyright@2024 All Rights Reserved, Andrea Visani, Hussein Majed & Anugrah Rai</p>
-    </div>
-</footer
